c28039e80add92fa4137dc9964e92ea74c29e9e5
Frederic789 <113332830+Frederic789@users.noreply.github.com>
2023-08-26 10:21:41 -0700

    Update QuizVM.swift

--- Chapter-MVVM-Intro/ViewModel/QuizVM.swift
+++ Chapter-MVVM-Intro/ViewModel/QuizVM.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+class QuizViewModel: ObservableObject {
+    @Published private var model: Quiz
+
+    init(model: Quiz) {
+        self.model = model
+    }
+
+    // Exposed properties and methods
+    var questions: [String] {
+        return model.questions
+    }
+
+    @Published var currentQuestionIndex: Int = 0
+
+    func getNextQuestion() {
+        // Increment the currentQuestionIndex to move to the next question
+        currentQuestionIndex += 1
+        
+        // Check if the index goes out of bounds and wrap back to the first question if needed
+        if currentQuestionIndex >= questions.count {
+            currentQuestionIndex = 0
+        }
+    }
+}
+
